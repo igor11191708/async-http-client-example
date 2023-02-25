f0ce0b737ce751afa6b229f2b3c2e4bf2fd6193e
Igor <igorshelopaev@gmail.com>
2023-02-25 10:51:39 +0500

    update

--- async-http-client-example/TaskDelegate.swift
+++ async-http-client-example/TaskDelegate.swift
@@ -6,8 +6,9 @@
 //
 
 import Foundation
+import async_http_client
 
-class TaskDelegate : NSObject, URLSessionTaskDelegate{
+class TaskDelegate : NSObject, ITaskDelegate{
     
     func urlSession(_ session: URLSession, task: URLSessionTask, didFinishCollecting metrics: URLSessionTaskMetrics) {
         print(metrics, "Metrics")
